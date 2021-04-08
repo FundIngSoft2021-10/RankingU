7bc6ea948822311abf990611c8cf9a3cbf71987a
Daniel Morales <danisan98@hotmail.es>
2021-04-07 22:05:42 -0500

    Paso de master a main
    Paso de master branch a main branch

---
+++ app/src/main/java/com/example/rankingu/ui/SignUp_Profesor_AgregarMateriasSuccess.java
@@ -0,0 +1,25 @@
+package com.example.rankingu.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.rankingu.R;
+
+public class SignUp_Profesor_AgregarMateriasSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up__profesor__agregar_materias_success);
+    }
+
+    public void changeToAgregarMaterias(View view)
+    {
+        Intent addMaterias = new Intent(this,SignUp_Profesor_AgregarMaterias.class);
+        startActivity(addMaterias);
+    }
+
+}
