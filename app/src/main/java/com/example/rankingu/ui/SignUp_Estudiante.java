8c8098cae0d46bd48e487c1c52499091383c351d
Daniel Morales <danisan98@hotmail.es>
2021-04-07 22:05:42 -0500

    Paso de master a main
    Paso de master branch a main branch

---
+++ app/src/main/java/com/example/rankingu/ui/SignUp_Estudiante.java
@@ -0,0 +1,24 @@
+package com.example.rankingu.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.rankingu.R;
+
+public class SignUp_Estudiante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up__estudiante);
+    }
+
+    public void changeToMain(View view)
+    {
+        Intent mainScreen = new Intent(this,mainScreen.class);
+        startActivity(mainScreen);
+    }
+}
---
+++ app/src/main/java/com/example/rankingu/ui/landing.java
@@ -0,0 +1,41 @@
+package com.example.rankingu.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.rankingu.R;
+
+public class landing extends AppCompatActivity {
+
+    private TextView iniciaSesion;
+    private TextView crearCuenta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+        iniciaSesion = (TextView)findViewById(R.id.textLogin);
+        crearCuenta = (TextView)findViewById(R.id.textSignUp);
+    }
+
+
+    public void changeToLogin(View view)
+    {
+        Intent login = new Intent(this, logIn.class);
+        startActivity(login);
+    }
+
+    public void changeToSignUp(View view)
+    {
+        Intent signup = new Intent(this, SignUp.class);
+        startActivity(signup);
+    }
+
+
+
+
+}
