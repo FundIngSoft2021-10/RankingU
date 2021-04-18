3a7ffa4041fca5cb1018f5b3a63f0a76247bbf6e
felipe becerra <afelipe.b20@gmail.com>
2021-04-18 18:13:13 -0500

    xd
    Signed-off-by: felipe becerra <afelipe.b20@gmail.com>

--- app/src/main/java/com/example/rankingu/ui/Settings/EditCorreoActivity.java
+++ app/src/main/java/com/example/rankingu/ui/Settings/EditCorreoActivity.java
@@ -1,16 +1,25 @@
 package com.example.rankingu.ui.Settings;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.rankingu.R;
 
 public class EditCorreoActivity extends AppCompatActivity {
+    Button b1;
+    EditText c1,c2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editarcorreo);
+        b1=(Button)findViewById(R.id.Botonconfirmareditcorreo);
+        c1=(EditText)findViewById(R.id.editTextTextEmailAddress);
+        c1=(EditText)findViewById(R.id.editTextTextEmailAddress2);
+
     }
+
 }
