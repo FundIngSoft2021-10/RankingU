e9c4fd48e3b20f1759a67e2bee988a09bf00e70e
Nicolas Gil <nicolas_gil@javeriana.edu.co>
2021-04-19 03:24:47 -0500

    Revert "clase reseña"
    This reverts commit 12d2c3aa6b6b6d01826ef034e48c866fbfde92eb.

--- app/src/main/java/com/example/rankingu/Classes/Materia.java
+++ app/src/main/java/com/example/rankingu/Classes/Materia.java
@@ -4,55 +4,21 @@ import java.util.List;
 
 public class Materia {
 
-<<<<<<< Updated upstream
     String nombre;
     Integer puntaje, semestre, cupos;
     List<String> horarios;
     List<String> profesores;
-=======
-    String nombre, descripcion;
-    Integer semestre;
-    Double rating;
-    Horario horarios;
-    List<String> profesoresList;
-    List<Reseña> reseñas;
->>>>>>> Stashed changes
 
     public Materia() {
     }
 
-<<<<<<< Updated upstream
     public Materia(String nombre, Integer puntaje, Integer semestre, Integer cupos, List<String> horarios, List<String> profesores) {
-=======
-    public Materia(String nombre, String descripcion, Integer semestre, Double rating, Horario horarios, List<String> profesoresList, List<Reseña> reseñas) {
->>>>>>> Stashed changes
         this.nombre = nombre;
         this.puntaje = puntaje;
         this.semestre = semestre;
         this.cupos = cupos;
         this.horarios = horarios;
-<<<<<<< Updated upstream
         this.profesores = profesores;
-=======
-        this.profesoresList = profesoresList;
-        this.reseñas = reseñas;
-    }
-
-    public List<Reseña> getReseñas() {
-        return reseñas;
-    }
-
-    public void setReseñas(List<Reseña> reseñas) {
-        this.reseñas = reseñas;
-    }
-
-    public Double getRating() {
-        return rating;
-    }
-
-    public void setRating(Double rating) {
-        this.rating = rating;
->>>>>>> Stashed changes
     }
 
     public String getNombre() {
