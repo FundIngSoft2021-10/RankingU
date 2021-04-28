604180ec58d75cb765aabccf4c38dfd26a8f122f
andreyton <48575992+andreyton@users.noreply.github.com>
2021-04-28 12:31:55 -0500

    Cambios

--- app/src/main/java/com/example/rankingu/ui/MainActivity.java
+++ app/src/main/java/com/example/rankingu/ui/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.rankingu.ui;
 //CLASE NECESARIA
+
 import com.example.rankingu.R;
 // MAYORMENTE INTERFAZ OSEA CLIENTE SUPONGO
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+
 import com.bumptech.glide.Glide;
+import com.example.rankingu.ui.Enroll.ConflictActivity;
+import com.example.rankingu.ui.Enroll.EnrollActivity;
+import com.example.rankingu.ui.Materia.MateriaActivity;
 import com.example.rankingu.ui.Search.SearchActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.NavController.OnDestinationChangedListener;
@@ -25,12 +35,18 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
 import com.google.android.material.navigation.NavigationView;
+
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.Menu;
+import android.view.View.OnLongClickListener;
+import android.view.Window;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -58,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
     private TableRow f7;
     private ImageView fotoUser;
     private TextView nombreUser;
+    private ImageView image;
 
     private String imgDef = "";
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -77,31 +94,55 @@ public class MainActivity extends AppCompatActivity {
         nombreUser = hView.findViewById(R.id.nombreView);
         fotoUser = hView.findViewById(R.id.fotoView);
         emailUser.setText(user.getEmail());
+        image = (ImageView) findViewById(R.id.goProDialogImage);
         matrizHorario = new ArrayList<>(12);
-        for(ArrayList<TextView> i:matrizHorario)
-        {
+        for (ArrayList<TextView> i : matrizHorario) {
             matrizHorario.add(new ArrayList<TextView>(8));
         }
-        if(user.getPhotoUrl()!=null && user.getDisplayName()!=null){
+        if (user.getPhotoUrl() != null && user.getDisplayName() != null) {
             Glide.with(this.getApplicationContext()).load(user.getPhotoUrl()).fitCenter().into(fotoUser);
             nombreUser.setText(user.getDisplayName());
-        }
-        else{
+        } else {
             consultaLogin(this.getApplicationContext());
         }
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send , R.id.nav_error)
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_error)
                 .setDrawerLayout(drawer)
                 .build();
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-               .findFragmentById(R.id.nav_host_fragment);
+                .findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
         //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        fotoUser.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setPositiveButton("Get Pro", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).setNegativeButton("No thanks", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        image.setImageResource(R.drawable.borde_horario);
+                    }
+                });
+                final AlertDialog dialog = builder.create();
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogLayout = inflater.inflate(R.layout.activity_cambio_img, null);
+                dialog.setView(dialogLayout);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+                dialog.show();
+                return true;
+            }
+        });
 
         navigationView.getMenu().findItem(R.id.nav_exit).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
@@ -135,7 +176,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Consulta
-    public void consultaLogin(final Context x){
+    public void consultaLogin(final Context x) {
         db.collection("Usuarios").document(user.getEmail()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
